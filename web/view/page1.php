ba1beda1871e720f59b4108f97883d6173e43483
Shaela <sut16006@byui.edu>
2018-05-03 10:03:27 -0600

    add empty link

--- web/view/page1.php
+++ web/view/page1.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang ="en">
   <head>
@@ -32,18 +31,14 @@
             <ul>
                 <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
                 <li><a href="hello.html">Home</a></li>
-                <li><a href="/view/page1.php">Work</a></li>
+                <li><a href="/view/myWork.html">Work</a></li>
             </ul>
           </nav>
       </header>
-      
-      <main id="home">
-          <div id='myBlah'>
-              <img src="/images/issaMe.jpg">
-              <h2 id='mahName'>This is me</h2>
-              <p>I'm Shaela, the little girl in the photo is my little girl who means the world to me. Her name is Penelope but we call her Penny for short. One of my hobbies is I love to embroider. I picked it up on a whim and am totally addicted to it now and am always looking for something else to stitch. I've really enjoyed all my web development classes so far and I'm excited about this class and all the important things we will learn this track!</p>
-          </div>
-      </main>
-  
+        
+        <main>
+            <h1>Coming Soon!</h1>
+        </main>
+        
     </body>
 </html>
