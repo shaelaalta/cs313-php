4c275b03ff794edfc6f370cc5b8247363f8be1d4
Shaela <sut16006@byui.edu>
2018-05-22 10:25:21 -0600

    connecting diff approach 1

--- web/organize/connection.php
+++ web/organize/connection.php
@@ -10,9 +10,22 @@ function connect()
 {   
     try
     {
+        /*$dbUrl = getenv('postgres://aaxshfcnahrbwi:ff8800c7b186b1134b1b5059e5306d47926abf3599e6fba861d9a10555cc0ecc@ec2-23-23-130-158.compute-1.amazonaws.com:5432/dbilarss332cbp');
+        $dbopts = parse_url($dbUrl);
         $user= 'aaxshfcnahrbwi';
         $password = 'ff8800c7b186b1134b1b5059e5306d47926abf3599e6fba861d9a10555cc0ecc';
-        $db = new PDO('pgsql:host=127.0.0.1;dbname=dbilarss332cbp', $user, $password);
+        $db = new PDO($dbopts, $user, $password);
+        return $db;*/
+        
+        $dbUrl = getenv('DATABASE_URL');
+        $dbopts = parse_url($dbUrl);
+        $dbHost = $dbopts["host"];
+        $dbPort = $dbopts["port"];
+        $dbUser = $dbopts["user"];
+        $dbPassword = $dbopts["pass"];
+        $dbName =  ltrim($dbopts["path"],'/');
+
+        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
         return $db;
     }
     catch (PDOException $ex)
