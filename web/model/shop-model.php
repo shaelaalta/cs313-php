1b823a376b24bb9ac05578fd5a8a911193478f76
Shaela <sut16006@byui.edu>
2018-05-21 23:03:38 -0600

    connecting to db take4

--- web/model/shop-model.php
+++ web/model/shop-model.php
@@ -5,7 +5,7 @@ function getProducts(){
     $sql = 'SELECT invId, invName, invPrice FROM inventory ORDER BY invName ASC';
     $stmt = $db->prepare($sql);
     $stmt->execute();
-    $prodList = $stmt->fetchAll();
+    $prodList = $stmt->fetchAll(PDO::FETCH_ASSOC);
     $stmt->closeCursor();
     return $prodList;
 }
