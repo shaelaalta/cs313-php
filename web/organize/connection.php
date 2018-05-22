b40c034c641e5f82f0179c5486f00792ed26525b
Shaela <sut16006@byui.edu>
2018-05-21 23:22:11 -0600

    connecting to db take5.3

--- web/organize/connection.php
+++ web/organize/connection.php
@@ -4,6 +4,7 @@
  * PDO Attempt, wish me luck
  * Shaela Sutton
  *host=67.60.89.156
+ * host=ec2-23-23-130-158.compute-1.amazonaws.com
  */
 function connect()
 {   
@@ -11,7 +12,7 @@ function connect()
     {
         $user= 'aaxshfcnahrbwi';
         $password = 'ff8800c7b186b1134b1b5059e5306d47926abf3599e6fba861d9a10555cc0ecc';
-        $db = new PDO('pgsql:host=ec2-23-23-130-158.compute-1.amazonaws.com;dbname=dbilarss332cbp', $user, $password);
+        $db = new PDO('pgsql:host=67.60.89.156;dbname=dbilarss332cbp', $user, $password);
         return $db;
     }
     catch (PDOException $ex)
