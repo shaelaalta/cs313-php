06901d514073fb9da600b8ede7cdd25e8bbc6d11
Shaela <sut16006@byui.edu>
2018-05-22 10:46:28 -0600

    connecting diff approach 4.1

--- web/model/shop-model.php
+++ web/model/shop-model.php
@@ -3,9 +3,9 @@
 function getProducts(){
     $db = connect();
     $sql = 'SELECT invId, invName, invPrice FROM inventory';
-    //$stmt = $db->prepare($sql);
-    $stmt = $db->query($sql);
-    //$stmt->execute();
+    $stmt = $db->prepare($sql);
+    //$stmt = $db->query($sql);
+    $stmt->execute();
     $prodList = $stmt->fetchAll(PDO::FETCH_ASSOC);
     //$stmt->closeCursor();
     return $prodList;
