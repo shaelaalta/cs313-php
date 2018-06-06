48d45d9112b4541167491fe32376d46b40212950
Shaela <sut16006@byui.edu>
2018-06-06 13:28:06 -0600

    book a ps.6

--- web/srProject/db/youknow.php
+++ web/srProject/db/youknow.php
@@ -9,10 +9,9 @@ function availableSched($times){
     $pd = '<div id="group">';
     foreach($times as $time){
         $schedDay = date_format($time[day], 'l js F Y');
-        echo $schedDay;
         $schedTimeS = $time[timestart];
         $schedTimeE = $time[timeend];
-        $pd .= "$schedDay from ". date_format($schedTimeS, 'g:i A') . "to ". date_format($schedTimeE, 'g:i A');
+        $pd .= $schedDay. " from ". date_format($schedTimeS, 'g:i A') . "to ". date_format($schedTimeE, 'g:i A');
         $pd .= "<a href='../sched/schedIndex.php?action=bookTime'>Schedule</a><br>";
     }
     $pd .= "</div>";
