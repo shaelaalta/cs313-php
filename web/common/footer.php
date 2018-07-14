d10519e9ac4c6b446cfcfbaec4f0ae25f21ee943
Shaela <sut16006@byui.edu>
2018-07-14 16:08:52 -0600

    footer fix.2

--- web/common/footer.php
+++ web/common/footer.php
@@ -1,3 +1,4 @@
+<div class="push"></div>
 <footer>
     <p>All of our products have free shipping and handling within the states, outside of the states shipping and handling costs $15.00</p>
     <a href="../view/admin.php">Administrator</a>
